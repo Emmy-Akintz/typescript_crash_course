a4207144879d4422170524e5178b3de98bb22051
Ayomide Akintan <emzyakints2005@gmail.com>
2023-11-22 08:19:13 +0100

    update

---
+++ 1.1-intro.ts
@@ -0,0 +1,42 @@
+//----------------
+// Typescript 101
+//----------------
+
+let myname = 'shaun'
+
+
+
+//--------------------------
+// Benefits of Typescript
+//--------------------------
+
+// 1 - better error feedback
+
+function reverse(str: string) {
+    return str.split('').reverse().join('')
+}
+
+const result = reverse('hello')
+
+// 2 - better autocompletion & code hints
+
+const reversed = reverse('ninja')
+
+// 3 - custom types
+
+interface MenuItem {
+    title: string
+    cost: number
+}
+
+function printMenuItem(item: MenuItem) {
+    console.log(item.title, ':', item.cost)
+}
+
+// error example 1
+// printMenuItem()
+
+// error example 2
+// printMenuItem({ title: 'veggie carbonara', cost: '15'})
+
+// error example 3
