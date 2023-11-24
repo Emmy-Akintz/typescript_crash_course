334462dd4de782d42bd08afe1a5227a4fec1ee23
Ayomide Akintan <emzyakints2005@gmail.com>
2023-11-24 11:32:55 +0100

    update

--- index.ts
+++ index.ts
@@ -1,3 +1,24 @@
-let age = 30
+let age: number = 30
+let firstName: string = 'Mario'
+let isFictional: boolean
 
-console.log(age);
+age = 31
+firstName = 'Luiji'
+isFictional = false
+
+let planet = 'Earth'
+let moons = 1
+let isLarge = false
+
+planet = 'Saturn'
+moons = 145
+isLarge = true
+
+// null & undefined
+
+let something: null
+let anotherThing: undefined
+
+something = null
+
+anotherThing = undefined
